f3c30a6a345493023b4912d68325229b46956f03
knavero <knavero@knavero-AO756>
2013-02-20 03:28:50 -0800

    Begin iterator implementation

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.util.Iterator;
 
 public class HashTable {
 
@@ -14,4 +15,28 @@ public class HashTable {
       BigInteger val = new BigInteger(String.valueOf(collectionSize));
       HashEntry = new HashEntry[val.intValue()*2];
    }
+   
+   private class Iter implements Iterator {
+      int cursor = 0;
+      
+      @Override
+      public boolean hasNext() {
+         // TODO Auto-generated method stub
+         return false;
+      }
+
+      @Override
+      public Object next() {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public void remove() {
+         // TODO Auto-generated method stub
+         
+      }
+      
+   }
+   
 }
