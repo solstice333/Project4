f17e9930c1b7fdd17f6894af045a6bd8ef3ae65c
knavero <knavero@knavero-AO756>
2013-02-20 03:08:34 -0800

    HashTable v1

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,5 +1,17 @@
+import java.math.BigInteger;
 
-public class HashTable
-{
+public class HashTable {
 
+   private class HashEntry {
+      public Object element;
+      public boolean active;
+   }
+
+   private HashEntry[] HashEntry;
+   private int occupied;
+   
+   public HashTable(int collectionSize) {
+      BigInteger val = new BigInteger(String.valueOf(collectionSize));
+      HashEntry = new HashEntry[val.intValue()*2];
+   }
 }
---
+++ src/testPrime.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+public class testPrime {
+
+   public static void main(String[] args) {
+      BigInteger val = new BigInteger("55");
+      
+      System.out.println(val.nextProbablePrime());
+   }
+
+}
