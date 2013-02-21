23b7c3befeb32009fc8394c18ce5b65ad6963366
knavero <knavero@knavero-AO756>
2013-02-21 14:02:06 -0800

    recent updates

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,41 +1,82 @@
 import java.math.BigInteger;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class HashTable {
 
    private class HashEntry {
       public Object element;
       public boolean active;
+
+      public HashEntry(Object x) {
+         element = x;
+         active = true;
+      }
    }
 
-   private HashEntry[] HashEntry;
+   private HashEntry[] HashArray;
    private int occupied;
-   
+
+   private int hash(Object x) {
+      return Math.abs(x.hashCode())%HashArray.length;
+   }
+
    public HashTable(int collectionSize) {
       BigInteger val = new BigInteger(String.valueOf(collectionSize));
-      HashEntry = new HashEntry[val.intValue()*2];
+      HashArray = new HashEntry[val.intValue() * 2];
    }
-   
+
+   // Requires testing
    private class Iter implements Iterator {
       int cursor;
-      
+
+      public Iter() {
+         cursor = 0;
+         while (!HashArray[cursor].active) {
+            cursor++;
+         }
+      }
+
       @Override
       public boolean hasNext() {
-         return cursor < HashEntry.length;
+         return cursor < HashArray.length;
       }
 
       @Override
       public Object next() {
-         // TODO Auto-generated method stub
-         return null;
+         if (!hasNext())
+            throw new NoSuchElementException();
+
+         HashEntry val = HashArray[cursor];
+         while (!HashArray[cursor].active) {
+            cursor++;
+         }
+
+         return val;
       }
 
       @Override
       public void remove() {
-         // TODO Auto-generated method stub
-         
+         throw new UnsupportedOperationException();
       }
-      
    }
-   
+
+   public Object find(Object item) {
+      int index = findPosition(item);
+      return HashArray[index];
+   }
+
+   private int findPosition(Object x) {
+      int i = 0;
+      int hashValue = hash(x);
+      int index = hashValue;
+
+      while (HashArray[index] != null && !x.equals(HashArray[index].element)) {
+         i++;
+         index = (hashValue + i * i) % HashArray.length;
+      }
+
+      return index;
+   }
+
 }
---
+++ src/test.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class test {
+
+   public static void main(String[] args) {
+      int[] arr = new int[10];
+      
+      System.out.println(arr.length);
+   }
+}
+
+
+
+
